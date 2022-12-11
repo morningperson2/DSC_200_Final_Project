5303589ed3a6ba0a015aaa5ae65030c192eb8573
Paul <morningperson2@gmail.com>
2022-12-11 12:54:46 -0500

    Worked on merging for jobs

--- Group_6_Final.py
+++ Group_6_Final.py
@@ -1,8 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
 def part1():
     print("Part 1")
-    pass
+    
+    #df = pd.merge(pd.merge(pd.merge(pd.merge(part1_DS1(), part1_DS2(), on="Date"), part1_DS3(), on="Date"), part1_DS4(), on="Date"), part1_DS5(), on="Date")
+    #df.to_csv("group_6_covid.csv")
 
 def part1_DS1() -> pd.DataFrame:
     pass
@@ -21,13 +25,89 @@ def part1_DS5() -> pd.DataFrame:
 
 def part2():
     print("Part 2")
+    #df1 = part2_DS1()
+    #df2 = part2_DS2()
+
+    #df1 = df1[df1["job title"].str.contains(["Data", "DATA"]) == True]
+    df = pd.merge(part2_DS1(), part2_DS2(), how="outer", on=["job title", "company name", "city", "state"])
+    df = df[df["job title"].str.contains("Data|DATA|data") == True]
+    df.to_csv("group_6_dsc_jobs.csv", index=False)
     pass
 
 def part2_DS1() -> pd.DataFrame:
-    pass
+    url= 'https://www.simplyhired.com/search?q=data+science&l=&job=f6OiCClL7m7gOAQ9o5ElmNquyKreuYz_0b-j3kwtgZ4_mb69UV9KQQ'
+    html = requests.get(url)
+
+    soup = BeautifulSoup(html.content, "html.parser")
+
+    salary=[]
+    job_title=[]
+    company_name=[]
+    city=[]
+    state=[]
+    job_type=[]
+
+    for a in soup.find_all('a', class_='SerpJob-link card-link'):
+        job_title.append(a.text.strip())
+
+    for div in soup.find_all('div', class_=['SerpJob-metaInfoLeft']):
+        if div.text == '':
+            salary.append('Unknown')
+        else:
+            salary.append(div.text.strip('Estimated: ').strip('Quick Apply').strip('a year'))
+
+    for span in soup.find_all('span', class_='JobPosting-labelWithIcon jobposting-company'):
+        company_name.append(span.text.strip())
+
+    for span in soup.find_all('span', class_='JobPosting-labelWithIcon jobposting-location'):
+        location= span.text.strip()
+        # separates city and state
+        if ',' in location:
+            split = (location.split(", "))
+            # adds city data
+            city.append(split[0])
+            # adds state data
+            state.append(split[1])
+        else:
+            city.append(location)
+            state.append(location)
+
+    df = pd.DataFrame({'job title': job_title, 'company name': company_name, 'salary': salary, 'city': city, 'state': state})
+    return df
 
 def part2_DS2() -> pd.DataFrame:
-    pass
+    url= 'https://www.glassdoor.com/Job/data-science-jobs-SRCH_KO0,12.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=data%2520science'
+    html = requests.get(url)
+
+    soup = BeautifulSoup(html.content, "html.parser")
+
+    job_title= []
+    company_name= []
+    city= []
+    state= []
+
+    for li in soup.find_all('li', class_='react-job-listing'):
+        for div in li.find_all('div', class_='d-flex justify-content-between align-items-start'):
+            company_name.append(div.text.strip())
+        for a in li.find_all('a', class_='jobLink css-1rd3saf eigr9kq3'):
+            for span in a.find_all('span'):
+                job_title.append(span.text.strip())
+        for div in li.find_all('div', class_='d-flex flex-wrap css-11d3uq0 e1rrn5ka2'):
+            for span in div.find_all('span'):
+                location= span.text.strip()
+                # separates city and state
+                if ',' in location:
+                    split = (location.split(", "))
+                    # adds city data
+                    city.append(split[0])
+                    # adds state data
+                    state.append(split[1])
+                else:
+                    city.append(location)
+                    state.append(location)
+
+    df = pd.DataFrame({'job title': job_title, 'company name': company_name, 'city': city, 'state': state})
+    return df
 
 def part2_DS3() -> pd.DataFrame:
     pass
