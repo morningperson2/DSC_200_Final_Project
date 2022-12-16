5791c065c82e68f78f218414b61dc698b3ce8a8a
Paul <morningperson2@gmail.com>
2022-12-16 00:08:02 -0500

    Added and merged DS5

--- Group_6_Final.py
+++ Group_6_Final.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from csv import writer
 import numpy as np
+import slate3k as sl
 
 def part1():
     print("Part 1")
@@ -11,12 +12,15 @@ def part1():
     part1_DS3()
     ds3 = pd.read_csv("ds3.csv")
     ds4 = part1_DS4()
+    ds5 = part1_DS5()
 
-    ds3["date"] = pd.to_datetime(ds3["date"]).dt.strftime('="%m/%d/%Y"')
+    ds3["date"] = pd.to_datetime(ds3["date"]).dt.strftime('="%m-%d-%Y"')
+    #ds5["date"] = pd.to_datetime(ds5["date"], '%d %b %Y').dt.strftime('="%m-%d-%Y"')
 
     df = pd.merge(ds1, ds2, how="outer", on="date")
     df = pd.merge(df, ds3, how="outer", left_on=["date", "area"], right_on=["date", "state"])
     df = pd.merge(df, ds4, how="outer", on="date")
+    df = pd.merge(df, ds5, how="outer", left_on="date", right_on="date")
     df.drop_duplicates(inplace=True)
     df.mask(df == "", inplace=True)
     df.dropna(axis=0, thresh=5, inplace=True)
@@ -64,7 +68,57 @@ def part1_DS4() -> pd.DataFrame:
     return ds4
 
 def part1_DS5() -> pd.DataFrame:
-    pass
+    name = "National Chart Data _ The COVID Tracking Project.pdf"
+    with open(name, "rb") as pdfObject:
+        doc = sl.PDF(pdfObject)
+
+        date = []
+        new_cases = []
+        row = []
+        count = 0
+
+        for index, page in enumerate(doc):
+            #print(page.split("\n"))
+            data = page.split("\n")
+            if(index == 0):
+                dateLow = 21
+                dateHigh = 54
+                casesLow = 65
+                casesHigh = 132
+            elif(index == 1):
+                dateLow = 216
+                dateHigh = 253
+                casesLow = 263
+                casesHigh = 320
+            elif(index == 2):
+                dateLow = 423
+                dateHigh = 460
+                casesLow = 470
+                casesHigh = 507
+            elif(index >= 3):
+                dateLow += 103
+                dateHigh += 37
+                casesLow += 10
+                casesHigh += 37
+        
+
+            for element in data:
+                count += 1
+                if(count >= dateLow and count < dateHigh):
+                    date.append(element)
+                if(count >= casesLow and count < casesHigh):
+                    new_cases.append(element)
+        
+        for i in range(len(date)):
+            row.append([date[i], new_cases[i]])
+
+    ds5 = pd.DataFrame(row)
+    ds5.mask(ds5 == "", inplace=True)
+    ds5.dropna(axis=0, thresh=1, inplace=True)
+    ds5.drop([106], inplace=True)
+    ds5.rename(columns={0:"date",1:"new_cases"}, inplace=True)
+
+    return ds5
 
 def part2():
     print("Part 2")
